e22d46c4ffde6f6afc543bcce583a3ef8cf3003f
cas-sav <94647032+cas-sav@users.noreply.github.com>
2022-10-15 18:10:32 -0400

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
-//1. Adding a new item to the list of items: 
+function newItem() {	
+	//1. Adding a new item to the list of items: 
+	let li = $('<li></li>');
+	let inputValue = $('#input').val();
+	li.append(inputValue);
 
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
-
-if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     $('#list').append(li);
-   }
+	if (inputValue === '') {
+	     alert("You must write something!");
+	   } else {
+	     $('#list').append(li);
+	   }
 
 
-//2. Crossing out an item from the list of items:
+	//2. Crossing out an item from the list of items:
+	function crossOut() {
+		li.toggleClass("strike");
+	}
+	// toggleClass is a manipulation
 
-function crossOut() {
-	li.toggleClass("strike");
+	li.on("dbclick", fuction crossOut() {
+		li.toggleClass("strike");
+	})
+	// equivalent to eventListener
+
+
+	//3(i). Adding the delete button "X":
+	let crossOutButton = $('<crossOutButton></crossOutButton>');
+	crossOutButton.append(document.createTextNode('X'));
+	li.append(crossOutButton);
+
+
+	//3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+	crossOutButton.on("click", deleteListItem);
+		function deleteListItem(){
+			li.addClass("delete")
+		}
+
+	// 4. Reordering the items: 
+	$('#list').sortable();
+
 }
-// toggleClass is a manipulation
-
-li.on("dbclick", fuction crossOut() {
-	li.toggleClass("strike");
-})
-// equivalent to eventListener
-
-//3(i). Adding the delete button "X":
